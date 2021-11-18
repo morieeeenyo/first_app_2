d5d55aa7c4606ddb2c13dd33d870428f57f28188
togo-mentor <uint2435@gmail.com>
2021-11-19 07:21:06 +0900

    component読み込み

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MemoList></MemoList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MemoList from './components/MemoList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MemoList
   }
 }
 </script>
---
+++ src/components/MemoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="memo-list">
+    <button>モーダルを開く</button>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemoList',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
