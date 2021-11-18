58fdc730f2693ea1215c92d52c9de6e3d0249086
togo-mentor <uint2435@gmail.com>
2021-11-19 07:26:56 +0900

    buttonのスタイリング

--- src/components/MemoList.vue
+++ src/components/MemoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="memo-list">
-    <button>モーダルを開く</button>
+    <button id="modal_button">モーダルを開く</button>
 
 
   </div>
@@ -15,5 +15,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#modal_button {
+  padding: 5px 20px ;
+  background-color: aqua;
+  border: none;
+  border-radius: 2px;
+}
+
+#modal_button:hover {
+  cursor: pointer;
+  border: 1px solid black;
+}
 
 </style>
