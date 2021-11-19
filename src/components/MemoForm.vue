a85a33f8f0a1fe149a31846304b602c4a3fd7713
togo-mentor <uint2435@gmail.com>
2021-11-20 06:59:20 +0900

    フォーム送信部分

--- src/components/MemoForm.vue
+++ src/components/MemoForm.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <div class="modal-header">
-      <h2>Modal title</h2>
+      <h2>メモを投稿する</h2>
     </div>
     <div class="modal-body">
-      <p>you're reading this text in a modal!</p>
+      <p>ほれ、フォームやぞ</p>
+      <form @submit="handleSubmit" class="modal-form">
+        <input type="text" name="content" id="content" v-model="post.content"  />
+        <input type="button" value="送信" @click="handleSubmit" />
+      </form>
       <button v-on:click="hide">閉じる</button>
     </div>
   </div>
@@ -12,10 +16,29 @@
 
 <script>
 export default {
+  data: function () {
+    return {
+      post: {
+        content: ''
+      }
+    }
+  },
   methods: {
     hide: function () {
-      this.$emit('hide')
+      this.$emit("hide");
+    },
+    handleSubmit: function (e) {
+      e.preventDefault()
+      console.log(this.post.content)
     }
-  }
-}
+  },
+};
 </script>
+
+<style>
+.modal-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
